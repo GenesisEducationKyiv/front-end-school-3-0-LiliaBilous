cc87dac6b05c77f8ec77e95e012383f580f911fe
LiliaBilous <liliyabilous.email@gmail.com>
2025-06-08 00:34:58 +0300

    feat: update dependencies, install ts-belt

--- eslint.config.js
+++ eslint.config.js
@@ -14,6 +14,7 @@ export default defineConfig([
       parserOptions: {
         project: "./tsconfig.json",
         tsconfigRootDir: process.cwd(),
+        extraFileExtensions: [".vue"],
       },
     },
   },
