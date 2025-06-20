58571f77c5fd80d2aab3cfe1527f602b1f277a78
LiliaBilous <liliyabilous.email@gmail.com>
2025-06-20 21:32:10 +0300

    refactor: minor changes

--- src/features/filters/store/trackFilterStore.ts
+++ src/features/filters/store/trackFilterStore.ts
@@ -1,16 +1,14 @@
 import { O, pipe } from '@mobily/ts-belt'
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
-import type { TrackQuery } from '@/features/tracks/schema/trackSchema.ts'
+import type { TrackQuery } from '@/features/filters/schema/trackQuerySchema.ts'
 export const useTrackFilterStore = defineStore('trackFilter', () => {
-  // state
   const search = ref<string>('')
   const artist = ref<string>('')
   const genre = ref<string>('')
   const sort = ref<string>('')
   const page = ref<number>(1)
 
-  // actions
   function toQuery(): TrackQuery {
     return {
       page: page.value,
@@ -39,14 +37,12 @@ export const useTrackFilterStore = defineStore('trackFilter', () => {
   }
 
   return {
-    // state
     search,
     artist,
     genre,
     sort,
     page,
 
-    // actions
     toQuery,
     initFromQuery,
     resetFilters,
