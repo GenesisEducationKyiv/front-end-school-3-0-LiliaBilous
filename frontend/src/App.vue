b86ec0ecb91453206f42ed477b5781625bdabbeb
LiliaBilous <liliyabilous.email@gmail.com>
2025-06-27 23:39:18 +0300

    feat: enhance ActiveTrack component with transition effects and improved styling

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,22 +7,19 @@ const AsyncTracksView = defineAsyncComponent({
   loader: () => import('@/features/tracks/views/TracksView.vue'),
   delay: 200,
   timeout: 10000,
+  suspensible: true,
 })
 </script>
 
 <template>
   <div class="wrapper">
-    <!-- ActiveTrack -->
-    <ActiveTrack />
-    <!-- Hero section -->
     <section class="hero">
       <div class="hero__background"></div>
       <div class="hero__content">
+        <ActiveTrack />
         <h1 class="hero__title" data-testid="tracks-header">Track Manager</h1>
       </div>
     </section>
-
-    <!-- Tracks View -->
     <Suspense>
       <template #default>
         <AsyncTracksView />
@@ -32,28 +29,53 @@ const AsyncTracksView = defineAsyncComponent({
       </template>
     </Suspense>
     <ModalHost />
-    <!-- Footer -->
     <AppFooter />
   </div>
 </template>
 <style>
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  /* max-height: 100%; */
+  min-height: 100vh;
+  position: relative;
+  overflow: auto;
+  gap: 1rem;
+}
+
 .hero {
   position: relative;
   height: var(--hero-height);
   width: 100%;
   background: center / cover url(@/assets/image.webp) no-repeat;
+  border: 1px var(--color-accent-gold) solid;
 }
 
 .hero__content {
   position: relative;
   z-index: 2;
   text-align: center;
-  color: var(--white-color);
-  padding-top: 6rem;
+  color: var(--color-text-base);
+}
+
+.hero__background::after {
+  content: '';
+  position: absolute;
+  inset: 0;
+  /* backdrop-filter: blur(1px); */
+  z-index: 1;
+}
+
+.hero__content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 
 .hero__title {
-  font-size: 3em;
+  font-size: 4rem;
   font-weight: 700;
+  margin-top: 5rem;
 }
 </style>
