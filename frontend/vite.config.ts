f129bf9a58dfec97948d2603152f610431d58b59
LiliaBilous <liliyabilous.email@gmail.com>
2025-06-29 01:50:48 +0300

    fix: format and clean up vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,27 +7,53 @@ import { visualizer } from 'rollup-plugin-visualizer'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), visualizer({
-    open: true,
-    filename: 'bundle-report.html',
-    gzipSize: true,
-    brotliSize: true,
-  }),],
+  plugins: [
+    vue(),
+    visualizer({
+      open: true,
+      filename: 'bundle-report.html',
+      gzipSize: true,
+      brotliSize: true,
+    }),
+  ],
+
   server: {
     port: 3000,
     proxy: {
       '/api': 'http://localhost:8000',
     },
   },
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-    }
+    },
   },
+
   build: {
     sourcemap: true,
-  }
 
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('@apollo/client')) return 'apollo'
+            if (id.includes('graphql')) return 'graphql'
+            if (id.includes('zod')) return 'zod'
+            if (id.includes('neverthrow')) return 'neverthrow'
+            if (id.includes('@mobily/ts-belt')) return 'ts-belt'
+          }
+        },
+      },
+    },
+  },
+
+  optimizeDeps: {
+    exclude: [
+      'graphql/language/lexer',
+      'graphql/language/parser',
+      'graphql/utilities/stripIgnoredCharacters',
+    ],
+  },
 })
