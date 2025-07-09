e9dba7d5f4738e13e720154592ac18dc59a3640d
LiliaBilous <liliyabilous.email@gmail.com>
2025-07-09 12:42:03 +0300

    fix(constants): set default API_BASE to localhost if VITE_API_URL is not defined

--- frontend/src/shared/services/api.ts
+++ frontend/src/shared/services/api.ts
@@ -4,13 +4,13 @@ import type { Track } from '@/features/tracks/schema/trackSchema.ts'
 import type { TrackQuery, PaginatedTracks } from '@/features/filters/schema/trackQuerySchema.ts'
 import { API_BASE } from '@/shared/constants.ts'
 
+export const getTrackAudioUrl = (filename: string): string => `http://localhost:8000/api/files/${filename}`
+
 export const getGenres = (): Promise<Result<string[], Error>> =>
   fetchWrapper<string[]>(`${API_BASE}/genres`)
 
 export const getTrackBySlug = (slug: string): Promise<Result<Track, Error>> =>
   fetchWrapper<Track>(`${API_BASE}/tracks/${slug}`)
-
-export const getTrackAudioUrl = (filename: string): string => `${API_BASE}/files/${filename}`
 
 export const createTrack = (trackData: Omit<Track, 'id'>): Promise<Result<Track, Error>> =>
   fetchWrapper<Track>(`${API_BASE}/tracks`, {
