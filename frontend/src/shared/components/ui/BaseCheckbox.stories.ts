56a3106af0503743cef0a989a1687c27460b6c4d
LiliaBilous <liliyabilous.email@gmail.com>
2025-07-14 01:38:55 +0300

    feat: implement BaseCheckbox component

---
+++ frontend/src/shared/components/ui/BaseCheckbox.stories.ts
@@ -0,0 +1,59 @@
+import BaseCheckbox from '@/shared/components/ui/BaseCheckbox.vue'
+
+export default {
+    title: 'Shared/BaseCheckbox',
+    component: BaseCheckbox,
+    tags: ['autodocs'],
+    argTypes: {
+        checked: { control: 'boolean' },
+        dataTestid: { control: 'text' },
+        ariaLabel: { control: 'text' },
+        id: { control: 'text' },
+        default: { control: 'text' },
+    },
+    parameters: {
+        docs: {
+            description: {
+                component: `Компонент BaseCheckbox реалізує чекбокс, взятий з дизайн-системи Material 3 (https://m3.material.io) та стилізований під головну дизайн-систему проєкту.`,
+            },
+        },
+    },
+}
+
+const render = (args: {
+    checked?: boolean,
+    dataTestid: string,
+    ariaLabel: string,
+    id: string,
+    default?: string,
+}) => ({
+    components: { BaseCheckbox },
+    setup() {
+        return { args }
+    },
+    template: `
+    <BaseCheckbox 
+      v-bind="args"
+    >
+      {{ args.default }}
+    </BaseCheckbox>
+  `,
+})
+
+export const Default = {
+    args: {
+        checked: false,
+        default: 'Label text',
+        dataTestid: 'base-checkbox',
+        ariaLabel: 'Base Checkbox',
+    },
+    render,
+}
+
+export const Checked = {
+    args: {
+        checked: true,
+        default: 'Checked state',
+    },
+    render,
+}
