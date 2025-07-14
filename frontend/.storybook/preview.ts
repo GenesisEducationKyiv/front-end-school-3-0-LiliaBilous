3012e70c13fa9321a40932096109c0b98718e853
LiliaBilous <liliyabilous.email@gmail.com>
2025-07-14 14:38:00 +0300

    feat: add stories for TrackCard component

--- frontend/.storybook/preview.ts
+++ frontend/.storybook/preview.ts
@@ -1,5 +1,10 @@
 import type { Preview } from '@storybook/vue3-vite'
+import { setup } from '@storybook/vue3'
 import '../src/assets/main.css'
+import { createPinia } from 'pinia'
+setup((app) => {
+  app.use(createPinia())
+})
 
 const preview: Preview = {
   parameters: {
